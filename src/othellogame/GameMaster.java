37c15ab72a083331da87e7bf404c28f6984c2de9
knaka <knaka@Debie>
2014-11-03 22:36:55 +0900

    add a test to GameMaster Class

--- src/othellogame/GameMaster.java
+++ src/othellogame/GameMaster.java
@@ -1,24 +1,30 @@
 package othellogame;
 
 public class GameMaster {
-	private Player p1, p2;
+	private Player player1, player2;
 	
 	public GameMaster(){
-		p1 = PlayerFactory.getPlayer(PlayerFactory.TYPE_CPU, Player.SIDE_BLACK);	
-		p2 = PlayerFactory.getPlayer(PlayerFactory.TYPE_USER, Player.SIDE_WHITE);
+		player1 = PlayerFactory.getPlayer(PlayerFactory.TYPE_CPU, Player.SIDE_BLACK);	
+		player2 = PlayerFactory.getPlayer(PlayerFactory.TYPE_USER, Player.SIDE_WHITE);
 	}
+	public GameMaster(Player p1, Player p2) {
+		this.player1 = p1;
+		this.player2 = p2;
+	}
+	
 	public void start(){
 		Board bd = new Board();
 		
 		bd.prepareBoard();
 
-		p1.registBoard(bd);
-		p2.registBoard(bd);
+		player1.registBoard(bd);
+		player2.registBoard(bd);
 		
 		boolean end_game = false;
 		while(end_game == false){
-			p1.turn();
-			p2.turn();
+			player1.turn();
+			player2.turn();
+			end_game = true;
 		}
 	}
 }
--- tst/othellogame/GameMasterTest.java
+++ tst/othellogame/GameMasterTest.java
@@ -8,12 +8,12 @@ public class GameMasterTest extends TestCase {
 
 	@Test
 	public void testGM(){
-		Board bd = new Board();
 		Player p1 = new UserPlayer(Player.SIDE_BLACK);
 		Player p2 = new CpuPlayer(Player.SIDE_WHITE);
-		GameMaster gm = new GameMaster(); 
+		GameMaster gm = new GameMaster(p1, p2); 
 	
-		bd.prepareBoard();
-		
+		assertTrue(true);
+		gm.start();
+		assertTrue(false);
 	}
 }
